df54b2db6a0ea9ad5ec05a54d5e18a701388e583
Shivam Asija <shivam.asija@mandsconsulting.com>
2022-02-16 13:44:38 +0530

    Latest changes till checking on vdi

--- Back-End/UAR_WITH_REACT/Controllers/VMAuditKpiController.cs
+++ Back-End/UAR_WITH_REACT/Controllers/VMAuditKpiController.cs
@@ -20,18 +20,18 @@ namespace UAR_WITH_REACT.Controllers
             _context = context;
         }
 
-        // GET: api/VpApprovals
+        // GET: api/kpiData
         [HttpGet]
         public async Task<ActionResult<IEnumerable<VwAuditKpi>>> GetTblVpApprovals()
         {
             return await _context.VwAuditKpis.Where(b=> b.SystemId == "Tim").ToListAsync();
         }
 
-        //[HttpGet("Eclipse")]
-        //public async Task<ActionResult<IEnumerable<VwAuditKpi>>> GetTblVpApprovals(string target)
-        //{
-        //    return await _context.VwAuditKpis.Where(b => b.SystemId == "Tim").ToListAsync();
-        //}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<VwAuditKpi>>> GetTblVpApprovals(string id)
+        {
+            return await _context.VwAuditKpis.Where(b => b.SystemId == id).ToListAsync();
+        }
 
     }
 }
