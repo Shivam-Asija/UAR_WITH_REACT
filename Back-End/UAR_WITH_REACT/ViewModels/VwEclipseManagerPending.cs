7acc3462892bda8cd9e7e2f1c9b2185fbb7049e1
Shivam Asija <shivam.asija@mandsconsulting.com>
2022-02-21 13:31:25 +0530

    Eclipse Pending page

---
+++ Back-End/UAR_WITH_REACT/ViewModels/VwEclipseManagerPending.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace UAR_WITH_REACT.Models
+{
+    public partial class VwEclipseManagerPending
+    {
+        public long UARID { get; set; }
+        public string ECLIPSESYSTEM { get; set; }
+        public string ACCESS { get; set; }
+        public string EMPLOYEEID { get; set; }
+        public string EMPLOYEENAME { get; set; }
+        public string TITLE { get; set; }
+        public string MANAGER { get; set; }
+        public string MANAGERSTITLE { get; set; }
+        public string SYSTEMID { get; set; }
+    }
+}
